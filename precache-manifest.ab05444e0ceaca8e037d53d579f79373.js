aed04fbe435296562d66832220bb60fc7b544450
Pearl <gansuvd777@gmail.com>
2019-01-30 15:02:36 -0600

    Updates

---
+++ precache-manifest.ab05444e0ceaca8e037d53d579f79373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7d10b724316d5517be9",
+    "url": "/gansuvd777/clickygame/static/css/main.e2215746.chunk.css"
+  },
+  {
+    "revision": "c7d10b724316d5517be9",
+    "url": "/gansuvd777/clickygame/static/js/main.c7d10b72.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/gansuvd777/clickygame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e5dc5e558a9fdccea583",
+    "url": "/gansuvd777/clickygame/static/js/runtime~main.e5dc5e55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gansuvd777/clickygame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "51c8ff49846012208846ee389ead8a48",
+    "url": "/gansuvd777/clickygame/index.html"
+  }
+];
